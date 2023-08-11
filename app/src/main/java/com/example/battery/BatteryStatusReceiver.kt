5df94e2cf5d74e1d6f7e5f3a12d1cea961ca338e
luanntph25463 <luanntph25463@fpt.edu.vn>
2023-08-11 08:55:51 +0700

    update battrry and module blutooth , network

--- app/src/main/java/com/example/battery/BatteryStatusReceiver.kt
+++ app/src/main/java/com/example/battery/BatteryStatusReceiver.kt
@@ -1,38 +1,160 @@
 package com.example.battery
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.AdvertiseCallback
+import android.bluetooth.le.AdvertiseSettings
+import android.bluetooth.le.BluetoothLeAdvertiser
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.TrafficStats
+import android.net.wifi.WifiInfo
+import android.net.wifi.WifiManager
 import android.os.BatteryManager
+import android.os.Process
+import android.telephony.TelephonyManager
+import android.util.Log
+import java.lang.reflect.Field
 
 
-class BatteryStatusReceiver( private val listener :  BatteryListener) : BroadcastReceiver() {
+class BatteryStatusReceiver(private val listener: BatteryListener) : BroadcastReceiver() {
+    private lateinit var bluetoothAdapter: BluetoothAdapter
+
+
     interface BatteryListener {
         fun onBatteryLevelChanged(level: Int)
+        fun onBatterytemperature(level: Int)
+        fun onBatteryisChanging(isharing: Boolean)
+
+
+        fun onConnectionTypeChanged(wifi: String)
+        fun onSpeedChanged(speed: Float)
+        fun onRating(speed: String)
+        fun onProviderChanged(networkOperatorName: String)
+        fun BluttothName(name: String)
+
     }
 
     override fun onReceive(context: Context, intent: Intent) {
+
         if (intent.action == Intent.ACTION_BATTERY_CHANGED) {
+            // get battery
             val level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
             val scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+
             val batteryPercentage = (level.toFloat() / scale.toFloat() * 100).toInt()
 
+            // truyen batteryPercentage in onBatteryLevelChanged
             listener.onBatteryLevelChanged(batteryPercentage)
+
+
+            // truyen temperature in onBatterytemperature
+            val temperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10
+            listener.onBatterytemperature(temperature)
+
+            // get status
+            val status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
+            //
+            val isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                    status == BatteryManager.BATTERY_STATUS_FULL
+            listener.onBatteryisChanging(isCharging)
+
+
+            // network connection
+        }
+
+
+        // wifi
+
+        val connManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connManager.activeNetwork
+        val networkCapabilities = connManager.getNetworkCapabilities(network)
+
+
+
+        if (networkCapabilities != null) {
+            if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                listener?.onConnectionTypeChanged("Wi-Fi")
+               val  telephonyManager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+                // Get information about the current Wi-Fi connection
+                val signalStrength = telephonyManager.signalStrength
+                val gsmSignalStrength: Int = signalStrength?.gsmSignalStrength ?: 0
+                // Chuyển đổi giá trị sang đơn vị dBm
+                val rssi: Int = -113 + 2 * gsmSignalStrength
+
+                // Chia độ mạnh yếu của tín hiệu GSM
+                val signalLevel: String = when {
+                    rssi >= -50 -> "Mạnh"
+                    rssi >= -75 -> "Trung bình"
+                    rssi >= -90 -> "Yếu"
+                    else -> "Rất yếu"
+                }
+                listener?.onRating(signalLevel)
+
+                val networkOperatorName = telephonyManager.networkOperatorName
+                listener?.onProviderChanged(networkOperatorName)
+
+
+            } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                listener?.onConnectionTypeChanged("Mobile Data")
+            }
+
+            val uid = Process.myUid()
+            val rxBytes = TrafficStats.getUidRxBytes(uid)
+            val txBytes = TrafficStats.getUidTxBytes(uid)
+
+// Tính toán tốc độ mạng
+            val rxSpeedMbps = (rxBytes * 8 / 1024 / 1024).toFloat() // Tốc độ download (Mbps)
+            val txSpeedMbps = (txBytes * 8 / 1024 / 1024).toFloat() // Tốc độ upload (Mbps)
+            val txttb = (rxSpeedMbps + txSpeedMbps ) /2
+            listener?.onSpeedChanged(txttb)
+
+
+            val telephonyManager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+
+//            val telephonyManager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+//            val networkOperatorName = telephonyManager.networkOperatorName
+//            listener?.onProviderChanged(networkOperatorName)
+        }
+
+
+
+        // blutooth
+
+
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (bluetoothAdapter == null) {
+            // Bluetooth không được hỗ trợ trên thiết bị này
+        }else{
+            val bluetoothName = getBluetoothName()
+            listener?.BluttothName(bluetoothName)
         }
 
     }
+    private fun getBluetoothName(): String {
+        var bluetoothName = ""
+        try {
+            val bluetoothManagerField: Field? = BluetoothAdapter::class.java.getDeclaredField("mService")
+            bluetoothManagerField?.isAccessible = true
+            val bluetoothManagerService: Any? = bluetoothManagerField?.get(bluetoothAdapter)
 
-    companion object {
-        fun register(context: Context, listener: BatteryListener): BatteryStatusReceiver {
-            val receiver = BatteryStatusReceiver(listener)
-            val intentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
-            context.registerReceiver(receiver, intentFilter)
-            return receiver
+            if (bluetoothManagerService != null) {
+                val bluetoothNameField: Field? = bluetoothManagerService.javaClass.getDeclaredField("mName")
+                bluetoothNameField?.isAccessible = true
+                bluetoothName = bluetoothNameField?.get(bluetoothManagerService) as? String ?: ""
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
 
-        fun unregister(context: Context, receiver: BatteryStatusReceiver) {
-            context.unregisterReceiver(receiver)
-        }
+        return bluetoothName
     }
+
+
+
 }
+
+
--- app/src/main/java/com/example/battery/HomeFragment.kt
+++ app/src/main/java/com/example/battery/HomeFragment.kt
@@ -2,59 +2,56 @@ package com.example.battery
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.SharedPreferences
-import android.os.BatteryManager
 import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.app.NotificationCompat
-
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_home.img
+import kotlinx.android.synthetic.main.fragment_home.percent_in
+import kotlinx.android.synthetic.main.fragment_home.percent_in2
+import kotlinx.android.synthetic.main.fragment_home.percent_in3
+import kotlinx.android.synthetic.main.fragment_home.percent_in4
+import kotlinx.android.synthetic.main.fragment_home.wifi_in
+import kotlinx.android.synthetic.main.fragment_home.wifi_in2
+import kotlinx.android.synthetic.main.fragment_home.wifi_in3
+import kotlinx.android.synthetic.main.fragment_home.wifi_in4
 import java.util.concurrent.TimeUnit
 
 
-class HomeFragment : Fragment() {
-    private lateinit var batteryStatusReceiver: BatteryStatusReceiver
-    private lateinit var percent_in2: TextView
-    private lateinit var percent_in3: TextView
-    private lateinit var percent_in: TextView
-    private lateinit var percent_in4: TextView
-    private lateinit var img: ImageView
+class HomeFragment : Fragment(), BatteryStatusReceiver.BatteryListener  {
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var notificationManager: NotificationManager
 
+    private lateinit var batteryStatusReceiver: BatteryStatusReceiver
+
     private val channelId = "Pin notification"
     private val notificationId = 1
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        createNotificationChannel()
-        notificationManager =
-            requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        percent_in2 = view.findViewById(R.id.percent_in2)
-        percent_in3 = view.findViewById(R.id.percent_in3)
-        percent_in4 = view.findViewById(R.id.percent_in4)
-        percent_in = view.findViewById(R.id.percent_in)
-        img = view.findViewById(R.id.img)
-        // khoi tao  shared perference
-        sharedPreferences = requireContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
-        // Create an instance of BatteryStatusReceiver
-        batteryStatusReceiver = BatteryStatusReceiver()
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
 
-        // Obtain a valid Context object
+            // khoi tao notification
+            createNotificationChannel()
+            notificationManager =
+                requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        // Register the BroadcastReceiver
-        val filter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
-        requireContext().registerReceiver(batteryStatusReceiver, filter)
-    }
+
+
+            batteryStatusReceiver = BatteryStatusReceiver(this)
+            val intentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+            requireContext().registerReceiver(batteryStatusReceiver, intentFilter)
+
+            val intentFilters = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+            requireContext().registerReceiver(batteryStatusReceiver, intentFilters)
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -68,7 +65,93 @@ class HomeFragment : Fragment() {
         super.onDestroy()
         requireContext().unregisterReceiver(batteryStatusReceiver)
     }
-// lay time hien hien tai tru di time cu
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = "notification Pin"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+            val channel = NotificationChannel(channelId, channelName, importance)
+            val notificationManager =
+                requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+    // gui giu lieu len notification
+    // nhan vao gia tri isCharging kiem tra xem co dang sac hay k
+    // nhan gia tri so luong pin
+    private fun sendBatteryStatusNotification(batteryLevel: Int?) {
+        val builder = NotificationCompat.Builder(requireContext(), channelId)
+            .setSmallIcon(R.drawable.home)
+            .setContentTitle("Battery Status")
+            .setContentText("Battery level: $batteryLevel%")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+
+        // Nếu đang sạc, hiển thị thông báo về tình trạng sạc
+
+
+        notificationManager.notify(notificationId, builder.build())
+    }
+
+    override fun onBatteryLevelChanged( level: Int) {
+        percent_in.text = " $level %"
+
+        // send notification
+        sendBatteryStatusNotification(level)
+
+    }
+
+    override fun onBatterytemperature(temperature: Int) {
+        percent_in2.text = " $temperature °C"
+
+    }
+
+    override fun onBatteryisChanging(ischaring: Boolean) {
+        if (!ischaring) {
+                    percent_in3.text = "Not charging"
+                    img.setImageResource(R.drawable.baseline_battery_unknown_24)
+
+
+                    // luu thoi gian hien tai
+                } else {
+                    // luu thoi gian hien tai
+                    val currentTime = System.currentTimeMillis()
+                    saveChargingTime(currentTime)
+                    // get time charging with get getSavedChargingTime
+                    // lay thoi gian cu
+                    val lastChargingTime = getSavedChargingTime()
+                    percent_in4.text = lastChargingTime
+                    img.setImageResource(R.drawable.baseline_battery_charging_full_24)
+                    percent_in3.text = " Charging"
+                }
+
+    }
+
+    override fun onConnectionTypeChanged(wifi: String) {
+        wifi_in.text = wifi
+    }
+
+    override fun onSpeedChanged(speed: Float) {
+        wifi_in2.text = speed.toString()
+
+    }
+
+    override fun onRating(speed: String) {
+        wifi_in3.text = speed
+    }
+
+    override fun onProviderChanged(networkOperatorName: String) {
+        wifi_in4.text = networkOperatorName
+    }
+
+    override fun BluttothName(name: String) {
+        wifi_in4.text = name
+        Log.d("this","$name")
+
+    }
+
+    // luu vao sharedd preferences
     private fun convertTimeToString(time: Long): String {
         val currentTime = System.currentTimeMillis()
         val elapsedTime = currentTime - time
@@ -80,7 +163,8 @@ class HomeFragment : Fragment() {
             "Ít hơn 1 giờ trước"
         }
     }
-    // luu vao sharedd preferences
+
+
     private fun saveChargingTime(time: Long) {
         val editor = sharedPreferences.edit()
         editor.putLong("LastChargingTime", time)
@@ -96,80 +180,4 @@ class HomeFragment : Fragment() {
         return "Chưa có dữ liệu"
     }
     // khoi tao  notificationChannal
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channelName = "notification Pin"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-
-            val channel = NotificationChannel(channelId, channelName, importance)
-            val notificationManager =
-                requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
-    // gui giu lieu len notification
-    // nhan vao gia tri isCharging kiem tra xem co dang sac hay k
-    // nhan gia tri so luong pin
-    private fun sendBatteryStatusNotification(batteryLevel: Int?, isCharging: Boolean?) {
-        val builder = NotificationCompat.Builder(requireContext(), channelId)
-            .setSmallIcon(R.drawable.home)
-            .setContentTitle("Battery Status")
-            .setContentText("Battery level: $batteryLevel%")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setAutoCancel(true)
-
-        // Nếu đang sạc, hiển thị thông báo về tình trạng sạc
-        if (isCharging == true) {
-            builder.setContentText("Battery level: $batteryLevel% (Charging)")
-        }
-
-        notificationManager.notify(notificationId, builder.build())
-    }
-
-    private inner class BatteryStatusReceiver : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            if (intent?.action == Intent.ACTION_BATTERY_CHANGED) {
-                // lay so luong pin
-                val level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
-                val scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
-                val batteryPercentage = (level.toFloat() / scale.toFloat() * 100).toInt()
-                percent_in.text = " $batteryPercentage %"
-                // lay nhiet do may
-                val temperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10
-
-                val status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
-                val isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
-                        status == BatteryManager.BATTERY_STATUS_FULL
-                sendBatteryStatusNotification(batteryPercentage, isCharging)
-
-                if (!isCharging) {
-                    percent_in3.text = "Not charging"
-                    img.setImageResource(R.drawable.baseline_battery_unknown_24)
-
-                    img.setImageResource(R.drawable.baseline_battery_charging_full_24)
-                    percent_in3.text = " Charging"
-                    // luu thoi gian hien tai
-                } else {
-                    // luu thoi gian hien tai
-                    val currentTime = System.currentTimeMillis()
-                    saveChargingTime(currentTime)
-                    // get time charging with get getSavedChargingTime
-                    // lay thoi gian cu
-                    val lastChargingTime = getSavedChargingTime()
-                    percent_in4.text = lastChargingTime
-                }
-
-
-                if (level <= 20 && !isCharging) {
-//            ivBatteryWarning.visibility = View.VISIBLE
-                    percent_in3.text = " PiN Yeu"
-                } else {
-//            ivBatteryWarning.visibility = View.GONE
-
-                }
-
-                percent_in2.text = "$temperature °C "
-            }
-        }
-    }
 }
